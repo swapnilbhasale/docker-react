fb9750b5d212ea7d59d3acdab632f66d97f3f40f
Swapnil Sunilkumar Bhasale <sbhasale@Lenovo.com>
2021-02-14 16:56:37 -0500

    changes app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, I love to learn
         </a>
       </header>
     </div>
